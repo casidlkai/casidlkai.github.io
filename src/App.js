d70c5111431931e5a276175084706a190f58388c
casidlkai <1713647912@qq.com>
2024-01-20 14:54:11 +0800

    重新测试

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           我的第一个react项目
         </p>
